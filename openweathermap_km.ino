d472d14b2c0ccaad01f3e1dc72cd4ec4bd93678c
KM <sodp2@naver.com>
2017-11-14 23:42:35 +0900

    온도와 날짜 데이터 수집

--- openweathermap_km.ino
+++ openweathermap_km.ino
@@ -5,13 +5,23 @@
 #define LOCATION   String("Asan")
 #define PORT      80
 
-char SSID[] = "m620_24g";
-const char PASS[] = "digitallogic";
+char SSID[] = "will_204";
+const char PASS[] = "16111711";
 const char URL[] = "api.openweathermap.org";
 
 WiFiClient client;
 
 int establish_state = 0;
+
+String currentLine = "";
+String tempLine = "";
+String dateLine = "";
+
+boolean tempActive = false;
+boolean dateActive = false;
+
+int tempValue = 0;
+int monthValue = 0;
 
 void setup()
 {
@@ -42,6 +52,51 @@ void loop()
       {
          char c = client.read();
          Serial.print(c);
+
+         currentLine += c;
+
+         if (currentLine.endsWith("<temperature value="))
+         {
+            tempActive = true;
+         }
+         else if(currentLine.endsWith("<lastupdate value="))
+         {
+            dateActive = true;
+         }
+         
+         if (tempActive)
+         {
+            if(c != 'm')
+            {
+               tempLine += c;
+            }
+            else
+            {
+               tempActive = false;
+                
+               tempValue = string_to_int(tempLine) - 273;  // 절대온도 계산
+               Serial.println();
+               Serial.print("tempActive : ");
+               Serial.print(tempValue);
+               Serial.println("°C");
+            }
+         }
+         else if(dateActive)
+         {
+            if(c != 'T')
+            {
+                dateLine += c;
+            } 
+            else
+            {
+                dateActive = false;
+                
+                monthValue = month_substring(dateLine);
+                Serial.println();
+                Serial.print("dayActive : ");
+                Serial.println(monthValue);
+            }
+         }
       }
 
       if (!client.connected() &&
@@ -55,6 +110,12 @@ void loop()
          delay(10000);
 
          Serial.println("");
+         
+         // 메모리에 데이터 리셋
+         currentLine = "";
+         tempLine = "";
+         dateLine = "";
+         
          setup_client();
          get_from_client();
       }
@@ -86,6 +147,45 @@ void get_from_client()
 
    Serial.println();
    Serial.println("Weather information for " + LOCATION);
-
+   
    establish_state = 1;
+}
+
+int month_substring(String input)
+{
+   char tmpMonth[3];
+   int result;
+   
+   input = input.substring(7, 9);
+   Serial.println();
+   Serial.println(input);
+
+   input.toCharArray(tmpMonth, sizeof(tmpMonth));
+   
+
+   result = atoi(tmpMonth);
+
+   Serial.println();
+   Serial.println(result);
+   
+   return result;
+}
+
+int string_to_int(String input)
+{
+   int i = 2;         // =" 두개를 제외하고 시작
+   char changeVal[20];
+   int result;
+
+   while(input[i] != '"')
+   {
+      i++;
+   }
+
+   input = input.substring(2, i);
+
+   input.toCharArray(changeVal, sizeof(changeVal));
+
+   result = atoi(changeVal);
+   return result;
 }
